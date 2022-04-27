c95b4bcad703f9f4e7a69fcc9105c3c07b9523aa
Uri Gruda <uri2920@gmail.com>
2022-04-27 16:01:41 +0300

    minor visual changes in email preview

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -2,9 +2,6 @@ import { AppHeader } from './cmps/app-header.jsx'
 import { About } from './pages/app-about.jsx'
 import { Home } from './pages/app-home.jsx'
 import { AppKeep } from './apps/keep/pages/app-keep.jsx'
-// import { EmailApp } from './apps/mail/pages/email-app.jsx'
-// import { EmailDetails } from './apps/mail/pages/email-details.jsx'
-// import { Email } from './apps/mail/email-root.jsx'
 import { EmailRoot } from './apps/mail/email-root.jsx'
 
 const Router = ReactRouterDOM.HashRouter
@@ -14,9 +11,6 @@ export function App() {
   return <Router>
     <AppHeader />
     <Switch>
-      {/* <Email /> */}
-      {/* <Route path="/email" component={EmailDetails} /> */}
-      {/* <Route path="/email/:emailId" component={EmailDetails} />*/}
       <Route path="/email" component={EmailRoot} />
       <Route path="/keep" component={AppKeep} />
       <Route path="/about" component={About} />
