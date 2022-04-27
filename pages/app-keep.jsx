457f8265acc7512b9f63945eeee02050f3d6022c
ShaharCohenVolos <shaharc4@gmail.com>
2022-04-27 11:24:53 +0300

    Test commit

---
+++ pages/app-keep.jsx
@@ -0,0 +1,5 @@
+export class AppKeep extends React.Component {
+    render(){
+        return console.log('hello')
+    }
+}
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,6 +1,7 @@
 import { AppHeader } from './cmps/app-header.jsx'
 import { About } from './pages/app-about.jsx'
 import { Home } from './pages/app-home.jsx'
+import {AppKeep} from './pages/app-keep.jsx'
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
@@ -9,8 +10,8 @@ export function App() {
   return <Router>
     <AppHeader />
     <Switch>
-      {/* <Route path="/mail" component={AppMail} />
-      <Route path="/keep" component={AppKeep} /> */}
+      {/* <Route path="/mail" component={AppMail} /> */}
+      <Route path="/keep" component={AppKeep} />
       <Route path="/about" component={About} />
       <Route path="/" component={Home} />
     </Switch>
