93f1aa5052dbe7122720f621377cf3ec90b79e14
Uri Gruda <uri2920@gmail.com>
2022-04-27 16:29:51 +0300

    revert restructuring

--- apps/mail/cmps/email-header.jsx
+++ apps/mail/cmps/email-header.jsx
@@ -1,7 +1,7 @@
 export class EmailHeader extends React.Component {
 
   render() {
-    return <header>
+    return <header className="email-header">
       <h1>Appsus Mail</h1>
       <input type="search" placeholder="Search mail" />
     </header>
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,8 +1,9 @@
 import { AppHeader } from './cmps/app-header.jsx'
 import { About } from './pages/app-about.jsx'
 import { Home } from './pages/app-home.jsx'
-import { KeepRoot } from './apps/keep/keep-root.jsx'
-import { EmailRoot } from './apps/mail/email-root.jsx'
+import { EmailApp } from './apps/mail/pages/email-app.jsx'
+import { EmailDetails } from './apps/mail/pages/email-details.jsx'
+import { AppKeep } from './apps/keep/pages/app-keep.jsx'
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
@@ -11,8 +12,9 @@ export function App() {
   return <Router>
     <AppHeader />
     <Switch>
-      <Route path="/email" component={EmailRoot} />
-      <Route path="/keep" component={KeepRoot} />
+      <Route path="/email/:emailId" component={EmailDetails} />
+      <Route path="/email" component={EmailApp} />
+      <Route path="/keep" component={AppKeep} />
       <Route path="/about" component={About} />
       <Route path="/" component={Home} />
     </Switch>
