4d7bea9942b7090a415ef0937a3e451b8bb2f6fe
Uri Gruda <uri2920@gmail.com>
2022-04-28 18:17:31 +0300

    adding the ability to send emails

--- apps/mail/cmps/email-folder-list.jsx
+++ apps/mail/cmps/email-folder-list.jsx
@@ -1,14 +1,11 @@
 const { NavLink } = ReactRouterDOM
 
 export function EmailFolderList() {
-  return <section className="email-folder-list">
-    <button className="compose-btn"><h1 className="plus"></h1><h1>Compose</h1></button>
-    <nav className="folder-nav">
-      <NavLink to="/email/inbox">Inbox</NavLink>
-      <NavLink to="/email/sent">Sent</NavLink>
-      <NavLink to="/email/trash">Trash</NavLink>
-      <NavLink to="/email/draft">Draft</NavLink>
-      <NavLink to="/email/all">All Mail</NavLink>
-    </nav>
-  </section>
+  return <nav className="email-folder-list">
+    <NavLink to="/email/inbox">Inbox</NavLink>
+    <NavLink to="/email/sent">Sent</NavLink>
+    <NavLink to="/email/trash">Trash</NavLink>
+    <NavLink to="/email/draft">Draft</NavLink>
+    <NavLink to="/email/all">All Mail</NavLink>
+  </nav>
 }
--- cmps/app-aside.jsx
+++ cmps/app-aside.jsx
@@ -1,9 +1,11 @@
+import { EmailCompose } from '../apps/mail/cmps/email-compose.jsx'
 import { EmailFolderList } from '../apps/mail/cmps/email-folder-list.jsx'
 
 const { Route } = ReactRouterDOM
 
 export function AppAside() {
   return <aside className="app-aside">
+    <Route path="/email" component={EmailCompose} />
     <Route path="/email" component={EmailFolderList} />
   </aside>
 }
