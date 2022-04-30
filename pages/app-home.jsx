28921a51794e6f713bb3e06666fc3b339bcedf1f
Uri Gruda <uri2920@gmail.com>
2022-04-30 21:08:17 +0300

    img fixes

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -4,7 +4,7 @@ export function Home() {
   return <main className="app-home no-aside-layout">
     <section className="welcome-banner">
       <div className="logo">
-        <img className="icon" src="../assets/img/logo.svg" alt="logo" />
+        <img className="icon" src="./../assets/img/logo.svg" alt="logo" />
         <h1 className="appsus">Appsus</h1>
       </div>
       <section className="get-started">
@@ -18,7 +18,7 @@ export function Home() {
     </section>
     <section className="card-container">
       <div className="card">
-        <div className="img-container"><img src="../assets/img/uri.jpg" alt="uri" /></div>
+        <div className="img-container"><img src="./../assets/img/uri.jpg" alt="uri" /></div>
         <h1>Uri Gruda</h1>
         <h2>Lead Email Designer</h2>
         <p>Hi, I'm Uri, currently working on becoming a fullstack developer. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa mollitia fugit, animi ex tempora commodi quo natus dolore tempore, accusamus repellendus optio distinctio magnam qui? Excepturi nihil ut facilis pariatur?
@@ -26,7 +26,7 @@ export function Home() {
           Maiores quia porro, dignissimos eligendi quam iusto eaque tempora consectetur reiciendis illum aut. Help I'm stuck in this photo. consequuntur nemo quo inventore id doloremque natus recusandae laboriosam dolore. Non nihil ipsam eligendi nulla consequatur?</p>
       </div>
       <div className="card">
-        <div className="img-container"><img src="../assets/img/shahar.jpg" alt="uri" /></div>
+        <div className="img-container"><img src="./../assets/img/shahar.jpg" alt="uri" /></div>
         <h1>Shahar Cohen</h1>
         <h2>Lead Keep Designer</h2>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa mollitia fugit, animi ex tempora commodi quo natus dolore tempore, accusamus repellendus optio distinctio magnam qui? Excepturi nihil ut facilis pariatur?
