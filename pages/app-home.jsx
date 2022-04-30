84d103f4b5a449ba5aed014dd8f8fb3c5d558ae1
ShaharCohenVolos <shaharc4@gmail.com>
2022-04-30 21:25:56 +0300

    last_fixes_for_github_pages

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -34,7 +34,7 @@ export function Home() {
         <p>Shahar Cohen Volos, age 29, from Haifa. I am merried +2 dogs. Before coming to Coding Academy
           I used to work for HP Computers.
           <br />
-          <br />
+         
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa mollitia fugit, animi ex tempora commodi quo natus dolore tempore, accusamus repellendus optio distinctio magnam qui? Excepturi nihil ut facilis pariatur?
           Vero minus voluptatem velit facilis id nihil. Consequatur eos sit quidem id maxime. Ea, quisquam ut molestias enim quos dignissimos! Accusantium molestias saepe minima qui, quibusdam possimus eaque ratione eos.
           Maiores quia porro, dignissimos eligendi quam iusto eaque tempora consectetur reiciendis illum aut blanditiis consequuntur nemo quo inventore id doloremque natus recusandae laboriosam dolore. Non nihil ipsam eligendi nulla consequatur?</p>
