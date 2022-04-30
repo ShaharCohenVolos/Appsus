038c81e53315ed95a76d5d3998b7ab743cd57572
Uri Gruda <uri2920@gmail.com>
2022-04-30 21:09:41 +0300

    img fixes

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -10,7 +10,7 @@ export function Home() {
         <h1 className="appsus">Appsus</h1>
       </div>
       <section className="get-started">
-        <h2>Let's get started...</h2>
+        <h2>Let's get started...test</h2>
         <div className="link-container" >
           <Link to="/book" className="book-link">Books</Link>
           <Link to="/email/inbox" className="email-link">Email</Link>
@@ -33,8 +33,8 @@ export function Home() {
         <h2>Lead Keep Designer</h2>
         <p>Shahar Cohen Volos, age 29, from Haifa. I am merried +2 dogs. Before coming to Coding Academy
           I used to work for HP Computers.
-          <br/> 
-          <br/> 
+          <br />
+          <br />
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa mollitia fugit, animi ex tempora commodi quo natus dolore tempore, accusamus repellendus optio distinctio magnam qui? Excepturi nihil ut facilis pariatur?
           Vero minus voluptatem velit facilis id nihil. Consequatur eos sit quidem id maxime. Ea, quisquam ut molestias enim quos dignissimos! Accusantium molestias saepe minima qui, quibusdam possimus eaque ratione eos.
           Maiores quia porro, dignissimos eligendi quam iusto eaque tempora consectetur reiciendis illum aut blanditiis consequuntur nemo quo inventore id doloremque natus recusandae laboriosam dolore. Non nihil ipsam eligendi nulla consequatur?</p>
