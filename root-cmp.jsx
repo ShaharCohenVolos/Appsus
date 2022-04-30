6f0adeaf7d77ce0f258496fd7d153da050e36c36
ShaharCohenVolos <shaharc4@gmail.com>
2022-04-30 12:38:24 +0300

    merge

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -20,8 +20,6 @@ export function App() {
     <Switch >
       <Route path="/email/:folder/:emailId" component={EmailDetails} />
       <Route path="/email/:folder/" component={EmailApp} />
-      {/* <Route path="/keep/edit/:keepId" component={KeepEdit} /> */}
-      {/* <Route path="/keep/:filter/:keepId" component={KeepDetails} /> */}
       <Route path="/keep/:filter" component={AppKeep} />
       <Route path="/keep" component={AppKeep} />
       <Route path="/book/:bookId" component={BookDetails} />
