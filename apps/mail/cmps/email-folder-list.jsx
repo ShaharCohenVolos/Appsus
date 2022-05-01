fb73469e57e83be3e272847f399216f2d59e50f6
Uri Gruda <uri2920@gmail.com>
2022-05-01 15:55:46 +0300

    spelling fix

--- apps/mail/cmps/email-folder-list.jsx
+++ apps/mail/cmps/email-folder-list.jsx
@@ -27,7 +27,7 @@ export class EmailFolderList extends React.Component {
       <NavLink to="/email/inbox" className="inbox"><div className="icon"></div>Inbox<h1 className="unread-count">{unread}</h1></NavLink>
       <NavLink to="/email/starred" className="starred"><div className="icon"></div>Starred</NavLink>
       <NavLink to="/email/sent" className="sent"><div className="icon"></div>Sent</NavLink>
-      <NavLink to="/email/draft" className="draft"><div className="icon"></div>Draft</NavLink>
+      <NavLink to="/email/draft" className="draft"><div className="icon"></div>Drafts</NavLink>
       <NavLink to="/email/trash" className="trash"><div className="icon"></div>Trash</NavLink>
       <NavLink to="/email/all" className="all-mail"><div className="icon"></div>All Mail</NavLink>
     </nav>
